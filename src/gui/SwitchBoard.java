5516ba5bd41a6bcd5eee96ffa2f8badf42c1403e
Daniel <db123@bergqvist.se>
2019-05-11 23:11:56 +0200

    Add comments

--- src/gui/SwitchBoard.java
+++ src/gui/SwitchBoard.java
@@ -22,36 +22,18 @@ public final class SwitchBoard {
 		
 		double thickness = 0.1;
 		
-//		components.add(new Line(0, 0, Component.SPACING, Component.SPACING, thickness, Color.BLACK));
-//		components.add(new Line(0, Component.SPACING, Component.SPACING, 0, thickness, LineStyle.SOLID));
-		
+		// Draw grid
 		for (int i=1; i*Component.SPACING < MainPanel.STALLVERK_HEIGHT; i++)
-//		for (int i=0; i < 100; i++)
 			components.add(new Line(0, i, (int)(MainPanel.STALLVERK_WIDTH/Component.SPACING), i, thickness, Color.LIGHT_GRAY));
 		
 		for (int i=1; i*Component.SPACING < MainPanel.STALLVERK_WIDTH; i++)
 			components.add(new Line(i, 0, i, (int)(MainPanel.STALLVERK_HEIGHT/Component.SPACING), thickness, Color.LIGHT_GRAY));
 		
+		// Draw board outline
 		components.add(new Line(0d, 0, MainPanel.STALLVERK_WIDTH, 0, thickness, Color.BLACK));
 		components.add(new Line(0d, MainPanel.STALLVERK_HEIGHT, MainPanel.STALLVERK_WIDTH, MainPanel.STALLVERK_HEIGHT, thickness, Color.BLACK));
 		components.add(new Line(0d, 0, 0, MainPanel.STALLVERK_HEIGHT, thickness, Color.BLACK));
 		components.add(new Line(MainPanel.STALLVERK_WIDTH, 0d, MainPanel.STALLVERK_WIDTH, MainPanel.STALLVERK_HEIGHT, thickness, Color.BLACK));
-/*		
-		components.add(new Line(0, 0, Component.SPACING, Component.SPACING, thickness, LineStyle.SOLID));
-		components.add(new Line(0, Component.SPACING, Component.SPACING, 0, thickness, LineStyle.SOLID));
-		
-		components.add(new Line(0d, 0, MainPanel.STALLVERK_WIDTH, 0, thickness, LineStyle.SOLID));
-		components.add(new Line(0d, MainPanel.STALLVERK_HEIGHT, MainPanel.STALLVERK_WIDTH, MainPanel.STALLVERK_HEIGHT, thickness, LineStyle.SOLID));
-		components.add(new Line(0d, 0, 0, MainPanel.STALLVERK_HEIGHT, thickness, LineStyle.SOLID));
-		components.add(new Line(MainPanel.STALLVERK_WIDTH, 0d, MainPanel.STALLVERK_WIDTH, MainPanel.STALLVERK_HEIGHT, thickness, LineStyle.SOLID));
-		
-		for (int i=0; i*Component.SPACING < MainPanel.STALLVERK_HEIGHT; i++)
-//		for (int i=0; i < 100; i++)
-			components.add(new Line(0, i, (int)(MainPanel.STALLVERK_WIDTH/Component.SPACING), i, thickness, LineStyle.DOTTED));
-		
-		for (int i=0; i*Component.SPACING < MainPanel.STALLVERK_WIDTH; i++)
-			components.add(new Line(i, 0, i, (int)(MainPanel.STALLVERK_HEIGHT/Component.SPACING), thickness, LineStyle.DOTTED));
-*/		
 	}
 	
 	public void draw(Graphics2D graphics)
